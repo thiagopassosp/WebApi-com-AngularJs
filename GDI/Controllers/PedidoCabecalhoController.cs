c8b65ad6199a115467553bc9cb12d1649b318234
Thiago <thiagopassosp@gmail.com>
2015-10-14 15:27:57 -0300

    Itens Pedido

--- GDI/Controllers/PedidoCabecalhoController.cs
+++ GDI/Controllers/PedidoCabecalhoController.cs
@@ -1,94 +1,17 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Net;
-using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Http.Description;
-using GDI.Models;
-namespace GDI.Controllers {
-    public class PedidoCabecalhoController : ApiController {
-        private BancoDadosEntities db = new BancoDadosEntities();
 
-        // GET: api/PedidoCabecalho
-        public IQueryable<pedido_cabecalho> Getpedido_cabecalho() {
-            return db.pedido_cabecalho;
-        }
-
-        // GET: api/PedidoCabecalho/5
-        [ResponseType(typeof(pedido_cabecalho))]
-        public async Task<IHttpActionResult> Getpedido_cabecalho(int id) {
-            pedido_cabecalho pedido_cabecalho = await db.pedido_cabecalho.FindAsync(id);
-            if (pedido_cabecalho == null) {
-                return NotFound();
-            }
-
-            return Ok(pedido_cabecalho);
-        }
-
-        // PUT: api/PedidoCabecalho/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> Putpedido_cabecalho(int id, pedido_cabecalho pedido_cabecalho) {
-            if (!ModelState.IsValid) {
-                return BadRequest(ModelState);
-            }
-
-            if (id != pedido_cabecalho.idpedido_cabecalho) {
-                return BadRequest();
-            }
-
-            db.Entry(pedido_cabecalho).State = EntityState.Modified;
-
-            try {
-                await db.SaveChangesAsync();
-            } catch (DbUpdateConcurrencyException) {
-                if (!pedido_cabecalhoExists(id)) {
-                    return NotFound();
-                } else {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/PedidoCabecalho
-        [ResponseType(typeof(pedido_cabecalho))]
-        public async Task<IHttpActionResult> Postpedido_cabecalho(pedido_cabecalho pedido_cabecalho) {
-            if (!ModelState.IsValid) {
-                return BadRequest(ModelState);
-            }
-
-            db.pedido_cabecalho.Add(pedido_cabecalho);
-            await db.SaveChangesAsync();
-
-            return CreatedAtRoute("DefaultApi", new { id = pedido_cabecalho.idpedido_cabecalho }, pedido_cabecalho);
-        }
-
-        // DELETE: api/PedidoCabecalho/5
-        [ResponseType(typeof(pedido_cabecalho))]
-        public async Task<IHttpActionResult> Deletepedido_cabecalho(int id) {
-            pedido_cabecalho pedido_cabecalho = await db.pedido_cabecalho.FindAsync(id);
-            if (pedido_cabecalho == null) {
-                return NotFound();
-            }
-
-            db.pedido_cabecalho.Remove(pedido_cabecalho);
-            await db.SaveChangesAsync();
-
-            return Ok(pedido_cabecalho);
-        }
-
-        protected override void Dispose(bool disposing) {
-            if (disposing) {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool pedido_cabecalhoExists(int id) {
-            return db.pedido_cabecalho.Count(e => e.idpedido_cabecalho == id) > 0;
+namespace GDI.Controllers
+{
+    public class PedidoCabecalhoController : Controller
+    {
+        // GET: PedidoCabecalho
+        public ActionResult Index()
+        {
+            return View();
         }
     }
 }
---
+++ GDI/Scripts/AngulasJSApp/pedidoCabecalhoController.js
@@ -0,0 +1,60 @@
+﻿//Cria o controller pedido cabeçalho
+app.controller('pedidoCabecalhoController', ['$scope', '$http', pedidoCabecalhoController]);
+
+//Método para o controller pedido cabeçalho
+function pedidoCabecalhoController($scope, $http) {
+
+    //Declaração de variáveis
+    $scope.ocultaTabelaListagempedidoCabecalhos = false;
+    $scope.exibeformAlterarpedidoCabecalho = false;
+
+    //Buscar listagem de pedido cabeçalho
+    $http.get('http://localhost/ForcaVendaWebAPI/api/PedidoCabecalho/Getpedido_cabecalho/').success(function (data) {
+        $scope.pedidoCabecalhos = data;
+    })
+    .error(function () {
+        $scope.error = "Erro ao carregar a listagem de pedidos!";
+    });
+
+    //Função para exibir o form para editar pedido
+    $scope.funcformEditarpedidoCabecalho = function () {
+        $scope.pedidoCabecalhoAlterar = this.pedidoCabecalho;
+        $scope.ocultaTabelaListagempedidoCabecalhos = true;
+        $scope.exibeformAlterarpedidoCabecalho = true;
+    };
+
+    //Ocultar o form de edição de pedido e exibir a listagem
+    $scope.funcCancelarEdicao = function () {
+        $scope.ocultaTabelaListagempedidoCabecalhos = false;
+        $scope.exibeformAlterarpedidoCabecalho = false;
+    };
+
+    //Editar o status do pedido cabeçalho
+    $scope.editarpedidoCabecalho = function () {
+        var ped = this.pedidoCabecalhoAlterar;
+        $http.put('http://localhost/ForcaVendaWebAPI/api/PedidoCabecalho/Putpedido_cabecalho/' + ped.idpedido_cabecalho, ped).success(function (data) {
+            alert("Pedido alterado com sucesso.");
+            $scope.ocultaTabelaListagempedidoCabecalhos = false;
+            $scope.exibeformAlterarpedidoCabecalho = false;
+        }).error(function (data) {
+            $scope.error = "Erro ao alterar o pedido! " + data;
+        });
+    };
+
+    //Deletar pedido
+    $scope.deletarpedidoCabecalho = function () {
+        var id = this.pedidoCabecalho.idpedido_cabecalho;
+        $http.delete('http://localhost/ForcaVendaWebAPI/api/PedidoCabecalho/Deletepedido_cabecalho/' + id).success(function (data) {
+            alert("Pedido deletado com sucesso.");
+            $.each($scope.pedidoCabecalhos, function (i) {
+                if ($scope.pedidoCabecalhos[i].idpedidoCabecalho === id) {
+                    $scope.pedidoCabecalhos.splice(i, 1);
+                    return false;
+                }
+            });
+        }).error(function (data) {
+            $scope.error = "Erro ao deletar o pedido." + data;
+        });
+    };
+
+}
